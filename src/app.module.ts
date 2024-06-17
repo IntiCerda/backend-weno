b0cf13da3d5134f1712df6958ceb8b67985868ab
inti cerda <inti.cerda@alumnos.ucn.cl>
2024-06-17 02:25:49 -0400

    feat: Add GraphQL support to the application

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,11 +6,21 @@ import { UserService } from './services/user.service';
 import { UserModule } from './modules/user.module';
 import { ServiceModule } from './modules/service.module';
 import { AuthModule } from './modules/auth.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
 
 
 @Module({
-  imports: [PrismaModule, UserModule, ServiceModule, AuthModule],
+  imports: [PrismaModule, UserModule, ServiceModule, AuthModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      sortSchema: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService, UserService],
 })
 export class AppModule {}
+ 
